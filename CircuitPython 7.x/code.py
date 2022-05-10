5c3f386e99f95a45b2a6e4ae6b7887626ad8740b
apiepho <alpiepho@gmail.com>
2022-05-10 11:31:56 -0600

    basic support for macropad using 6 keys

--- CircuitPython 7.x/code.py
+++ CircuitPython 7.x/code.py
@@ -1,29 +1,38 @@
 # SPDX-FileCopyrightText: 2021 Noe Ruiz for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
+#
 
 import time
-import digitalio
 import board
 import usb_hid
-import neopixel
 from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
-from adafruit_led_animation.animation.pulse import Pulse
-from adafruit_led_animation.animation.solid import Solid
-from adafruit_led_animation.group import AnimationGroup
-from adafruit_led_animation.sequence import AnimationSequence
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+from adafruit_macropad import MacroPad
 
-from adafruit_led_animation.color import WHITE, BLACK
+BRIGHTNESS_LOW = 0.2
+BRIGHTNESS_HIGH = 1.0
 
-pixel_pin = board.A3
-num_pixels = 7
+BLACK = 0x000000
+GREEN = 0x00FF00
+YELLOW = 0xFFFF00
+ORANGE = 0xFF8C00
+RED = 0xFF0000
 
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=.3, auto_write=False)
-internal_pixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=1, auto_write=False)
+GREEN_DIM = 0x002200
+YELLOW_DIM = 0x222200
+ORANGE_DIM = 0x221200
+RED_DIM = 0x220000
 
-# The button pins we'll use, each will have an internal pullup
-buttonpins = [board.A0, board.A1, board.A2, board.D8, board.D9, board.D10]
+macropad = MacroPad()
+
+for i in range(len(macropad.pixels)):
+    macropad.pixels[i] = BLACK
+macropad.pixels.brightness = BRIGHTNESS_HIGH
+
+buttonpins = [board.KEY1, board.KEY2, board.KEY3, board.KEY4, board.KEY5, board.KEY6]
 
 # The keycode sent for each button, will be paired with a control key
 buttonkeys = [
@@ -35,40 +44,25 @@ buttonkeys = [
     ConsumerControlCode.REWIND
 ]
 
-# the keyboard object!
 cc = ConsumerControl(usb_hid.devices)
-# our array of button objects
-buttons = []
 
-# make all pin objects, make them inputs w/pullups
-for pin in buttonpins:
-    button = digitalio.DigitalInOut(pin)
-    button.direction = digitalio.Direction.INPUT
-    button.pull = digitalio.Pull.UP
-    buttons.append(button)
-
-animations = AnimationSequence(
-    AnimationGroup(
-        Pulse(pixels, speed=0.05, color=WHITE, period=6),
-        Solid(internal_pixel,color=BLACK),
-    ),
-)
-
-print("Waiting for button presses")
+print("\n\nWaiting for button presses")
 
 while True:
-    # animate neopixel jewel
-    animations.animate()
     # check each button
-    for button in buttons:
-        if not button.value:  # pressed?
-            i = buttons.index(button)
+    event = macropad.keys.events.get()
+    if event:
+        i = event.key_number
+        if event.pressed:
+            macropad.pixels[i] = GREEN
+        if event.released:
+            print("\n\nButton #%d Pressed" % i)
 
-            print("Button #%d Pressed" % i)
-
-            while not button.value:
-                pass  # wait for it to be released!
-            # type the keycode!
-            k = buttonkeys[i]  # get the corresp. keycode
-            cc.send(k)
+            if i < len(buttonkeys):
+                k = buttonkeys[i]  # get the corresp. keycode
+                print('code: ' + str(k))
+                cc.send(k)
+            else:
+                print()
+            macropad.pixels[i] = BLACK
     time.sleep(0.01)
