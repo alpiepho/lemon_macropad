98c2ef5cc6eb960751c35512f2cae22f645ece8c
apiepho <alpiepho@gmail.com>
2022-05-11 06:21:44 -0600

    more progress, almost there

--- CircuitPython 7.x/code.py
+++ CircuitPython 7.x/code.py
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: MIT
 
 import time
-import board
 import usb_hid
 from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
@@ -28,35 +27,19 @@ BLUE = 0x0000FF
 
 class Button():
     number = 1
-    color = WHITE
+    colorOn = WHITE
+    colorOff = BLACK
     type = TYPE_CODE
+    codestr = ''
     value = Keycode.SPACE
 buttons = []
 
-macropad = MacroPad()
-
-for i in range(len(macropad.pixels)):
-    macropad.pixels[i] = BLACK
-macropad.pixels.brightness = BRIGHTNESS_HIGH
-
-
-# The keycode sent for each button, will be paired with a control key
-buttoncodes = [
-    ConsumerControlCode.PLAY_PAUSE,
-    ConsumerControlCode.FAST_FORWARD,
-    ConsumerControlCode.VOLUME_INCREMENT,
-    ConsumerControlCode.MUTE,
-    ConsumerControlCode.VOLUME_DECREMENT,
-    ConsumerControlCode.REWIND
-]
-
-# kbd = Keyboard(usb_hid.devices)
-
 keyboard = Keyboard(usb_hid.devices)
 layout = KeyboardLayoutUS(keyboard)
-
-
 cc = ConsumerControl(usb_hid.devices)
+macropad = MacroPad()
+
+
 
 def parse_color(s):
     if s.startswith('0x'):
@@ -119,11 +102,17 @@ def parse_code(s):
         print('WARNING: given code string ' + s + ' is not known, using MUTE.')
     return value
 
-def get_color(index):
+def get_colorOn(index):
     for b in buttons:
         if index+1 == b.number:
-            return b.color
+            return b.colorOn
     return WHITE
+
+def get_colorOff(index):
+    for b in buttons:
+        if index+1 == b.number:
+            return b.colorOff
+    return BLACK
 
 def get_button(index):
     for b in buttons:
@@ -131,42 +120,12 @@ def get_button(index):
             return b
     return None
 
-# read data.txt
-try:
-    with open('data.txt') as file:
-        lines = file.readlines()
-        for line in lines:
-            line = line.strip()
-            if line.startswith('#'):
-                continue
-            if len(line) == 0:
-                continue
-            print(line)
-            parts = line.split(':')
-            if len(parts) == 4:
-                b = Button()
-                b.number = int(parts[0].strip())
-                b.color = parse_color(parts[1].strip())
-                b.type = parse_type(parts[2].strip())
-                if b.type == TYPE_CODE:
-                    b.value = parse_code(parts[3].strip())
-                else:
-                    b.value = parts[3].replace('"', '').strip()
-                buttons.append(b)
-except Exception as err:
-    print(err)
-    print('Please provide data.txt with details about buttons.')
-
-
-print("\n\nWaiting for button presses")
-
-while True:
-    # check each button
+def check_keys():
     event = macropad.keys.events.get()
     if event:
         i = event.key_number
         if event.pressed:
-            macropad.pixels[i] = get_color(i)
+            macropad.pixels[i] = get_colorOn(i)
         if event.released:
             print("\n\nButton #%d Pressed" % i)
             b = get_button(i)
@@ -179,5 +138,74 @@ while True:
                     layout.write(b.value)
             else:
                 print()
-            macropad.pixels[i] = BLACK
+            macropad.pixels[i] = get_colorOff(i)
+
+
+last_position = macropad.encoder
+def check_encoder():
+    global last_position
+    if macropad.encoder != last_position:
+        index = abs(macropad.encoder) % len(buttons)
+        b = buttons[index]
+        if b.type == TYPE_CODE:
+            print(f'\n\n\n{b.number:2}: {b.codestr}')
+        else:
+            print(f'\n\n\n{b.number:2}: "{b.value}"')
+    last_position = macropad.encoder
+
+
+
+
+# read data.txt
+data_found = True
+try:
+    with open('data.txt') as file:
+        lines = file.readlines()
+        for line in lines:
+            line = line.strip()
+            if line.startswith('#'):
+                continue
+            if len(line) == 0:
+                continue
+            print(line)
+            parts = line.split(':')
+            if len(parts) == 5:
+                b = Button()
+                b.number = int(parts[0].strip())
+                b.colorOn = parse_color(parts[1].strip())
+                b.colorOff = parse_color(parts[2].strip())
+                b.type = parse_type(parts[3].strip())
+                if b.type == TYPE_CODE:
+                    b.codestr = parts[4].strip()
+                    b.value = parse_code(parts[4].strip())
+                else:
+                    b.value = parts[4].replace('"', '').strip()
+                buttons.append(b)
+except Exception as err:
+    data_found = False
+
+# update neopixel colors from data.txt
+for i in range(len(macropad.pixels)):
+    macropad.pixels[i] = get_colorOff(i)
+macropad.pixels.brightness = BRIGHTNESS_HIGH
+
+# starting message
+if data_found:
+    print("\n\nWaiting for button presses")
+else:
+    print('Please provide data.txt with details about buttons.')
+
+# main loop
+while True:
+    check_keys()
+    check_encoder()
     time.sleep(0.01)
+
+# TODO 
+# - refactor
+# - hold strings from data, convert when needed
+# - test mode?
+# - clear lcd icon
+# - README 
+# References - move to README
+# https://github.com/Neradoc/Circuitpython_Keyboard_Layouts
